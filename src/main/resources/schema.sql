0c4bcc0955c6fb3cc0f6633450b03a7af8aa1a14
Ayushi <ayushi0430@outlook.com>
2019-02-24 23:54:39 +0530

    Update schema and db properties

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS Products;
 CREATE TABLE Products(
     id serial PRIMARY KEY,
-    name VARCHAR(100),
-    price integer,
-    breadcrumbs VARCHAR(200),
-    storename VARCHAR(20)
+    name VARCHAR(100) NOT NULL,
+    price integer NOT NULL,
+    breadcrumbs VARCHAR(200) NOT NULL,
+    storename VARCHAR(20) NOT NULL
 );
