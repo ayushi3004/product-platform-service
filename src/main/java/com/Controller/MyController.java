abb7e0775185fb521b4867c73e6f0846b21fa30d
Ayushi <ayushi0430@outlook.com>
2019-02-24 21:31:42 +0530

    get by id, change response to responseEntity

--- src/main/java/com/Controller/MyController.java
+++ src/main/java/com/Controller/MyController.java
@@ -3,9 +3,11 @@ package com.Controller;
 import com.Bean.Product;
 import com.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -17,7 +19,12 @@ public class MyController {
     ProductService productService;
 
     @RequestMapping(method = GET, path = "/products")
-    public List<Product> getProducts(Model model) {
+    public ResponseEntity<List<Product>> getProducts() {
         return productService.getAllProducts();
+    }
+
+    @RequestMapping(method = GET, path = "/products/{id}")
+    public ResponseEntity<Product> getProductsById(@PathVariable(value = "id") Long id) {
+        return productService.getProductsById(id);
     }
 }
--- src/main/java/com/Service/ProductService.java
+++ src/main/java/com/Service/ProductService.java
@@ -1,9 +1,10 @@
 package com.Service;
 
 import java.util.List;
-
 import com.Bean.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,16 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return ResponseEntity.ok().body(products);
+    }
+
+    public ResponseEntity<Product> getProductsById(long id) {
+        Product product =
+                productRepository
+                        .findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException("Product with id:" + id + " not found"));
+        return ResponseEntity.ok().body(product);
     }
 }
