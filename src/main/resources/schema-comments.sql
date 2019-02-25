075afc466bf1cd525a84bebb376c0bdbbc6fe6c0
Ayushi <ayushi0430@outlook.com>
2019-02-25 15:56:23 +0530

    Move postgres commands for all tables to a single file

--- src/main/resources/schema-comments.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS Comments;
-CREATE TABLE Comments(
-    id serial PRIMARY KEY references products(id) FOREIGN KEY,
-    userName VARCHAR(100),
-    comment VARCHAR(500)
-);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,18 @@
-DROP TABLE IF EXISTS Products;
-CREATE TABLE Products(
-    id serial PRIMARY KEY,
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS Comment;
+
+CREATE TABLE Product(
+    productId serial PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     price integer NOT NULL,
     breadcrumbs VARCHAR(200) NOT NULL,
     storeName VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Comment(
+    commentId serial PRIMARY KEY,
+    userName VARCHAR(100),
+    message VARCHAR(500),
+    productId INTEGER,
+    CONSTRAINT fk_comment_product FOREIGN KEY ("productid") REFERENCES PRODUCT ("productid")
 );
